503c98eff4409b699ed5434a7b0ef0d21f3fe6e4
Necati Berimen <berimen66@gmail.com>
2022-03-15 18:30:59 +0300

    create other repository

--- src/main/java/com/nberimen/addressregistrationsystem/repository/AddressRepository.java
+++ src/main/java/com/nberimen/addressregistrationsystem/repository/AddressRepository.java
@@ -1,9 +1,25 @@
 package com.nberimen.addressregistrationsystem.repository;
 
+import com.nberimen.addressregistrationsystem.dto.AddressDetailDto;
 import com.nberimen.addressregistrationsystem.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+
+    @Query(
+            "select new com.nberimen.addressregistrationsystem.dto.AddressDetailDto(" +
+                    "country.name, city.name, district.name, nb.name, street.name" +
+                    ")" +
+                    "from Address adr left join Street street on adr.streetId = street.id " +
+                    "left join Neighborhood nb on nb.id = street.neighborhoodId " +
+                    "left join District district on district.id = nb.districtId " +
+                    "left join City city on city.id = district.cityId " +
+                    "left join Country country on country.id = city.countryId " +
+                    "where adr.id = :id "
+    )
+    AddressDetailDto findAddressDetails(@Param("id") Long id);
 }
--- src/main/java/com/nberimen/addressregistrationsystem/repository/NeighborhoodRepository.java
+++ src/main/java/com/nberimen/addressregistrationsystem/repository/NeighborhoodRepository.java
@@ -4,6 +4,10 @@ import com.nberimen.addressregistrationsystem.entity.Neighborhood;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NeighborhoodRepository extends JpaRepository<Neighborhood, Long> {
+
+    List<Neighborhood> findAllByDistrictId(Long districtId);
 }
